278e03b29f8770b79bf8c93c590c7a88871bed0d
jmlee3210 <jmlee321083@gmail.com>
2024-11-26 16:13:09 -0500

    .

---
+++ pages/csc309-e11.js
@@ -0,0 +1,9 @@
+export default function Csc309E11() {
+  const utorId = "__your_utorid__"; // Replace with your UtorID
+  return (
+    <div id="auto-tester">
+      Hi. I am {utorId}, and I have learned how to develop a full-stack web app
+      and deploy it!
+    </div>
+  );
+}
