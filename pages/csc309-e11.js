7ad1ae8fc17f06a17af9d5059c7cdead7f57e8f9
jmlee3210 <jmlee321083@gmail.com>
2024-11-26 16:18:12 -0500

    .

--- pages/csc309-e11.js
+++ pages/csc309-e11.js
@@ -1,5 +1,5 @@
 export default function Csc309E11() {
-  const utorId = "__your_utorid__"; // Replace with your UtorID
+  const utorId = "leejaem6";
   return (
     <div id="auto-tester">
       Hi. I am {utorId}, and I have learned how to develop a full-stack web app
---
+++ script.js
@@ -0,0 +1,12 @@
+function getBaseUrl() {
+  return "https://csc-309-e11.vercel.app/";
+}
+
+function getUTorId() {
+  return "leejaem6";
+}
+
+module.exports = {
+  getBaseUrl,
+  getUTorId,
+};
